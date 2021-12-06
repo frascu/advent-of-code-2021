a735d11c9c2eacd5e92ebda5d1e93e70fa63eefe
frascu <francescoscuccimarri@gmail.com>
2021-12-06 12:18:36 +0100

    change method name

--- src/main/java/it/frascu/adventofcode/Day06.java
+++ src/main/java/it/frascu/adventofcode/Day06.java
@@ -1,8 +1,6 @@
 package it.frascu.adventofcode;
 
-import java.io.InputStream;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Day06 {
@@ -10,12 +8,12 @@ public class Day06 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(Day06.class.getClassLoader().getResourceAsStream("Day06.txt"))) {
             List<Integer> fishes = Arrays.stream(scanner.nextLine().split(",")).map(Integer::valueOf).toList();
-            System.out.println("Part 1: " + getNumberOfFishesAfterDaysEfficient(fishes, 80));
-            System.out.println("Part 2: " + getNumberOfFishesAfterDaysEfficient(fishes, 256));
+            System.out.println("Part 1: " + getNumberOfFishesAfterDays(fishes, 80));
+            System.out.println("Part 2: " + getNumberOfFishesAfterDays(fishes, 256));
         }
     }
 
-    private static long getNumberOfFishesAfterDaysEfficient(List<Integer> fishes, int days) {
+    private static long getNumberOfFishesAfterDays(List<Integer> fishes, int days) {
         long[] counter = new long[9];
         fishes.forEach(fish -> counter[fish] += 1);
         IntStream.range(0, days).forEach(i -> {
